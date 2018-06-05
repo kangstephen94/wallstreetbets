72cacd0af7a213f8423b007bdcdf8c36540ad36c
Stephen <kang.stephen94@gmail.com>
2018-06-05 15:21:46 -0700

    Migrate users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  buying_power    :integer          not null
+#  session_token   :string           not null
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
 end
