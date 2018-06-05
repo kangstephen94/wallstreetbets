5f71d57a6260ab959a1712e1e43706fcf94be4dd
Stephen <kang.stephen94@gmail.com>
2018-06-05 16:02:00 -0700

    Implement user model, user and session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,38 @@
 #
 
 class User < ApplicationRecord
+  validates :first_name, :last_name, :password_digest, presence: true
+  validates :buying_power, numericality: { greater_than_or_equal_to: 0 }
+  validates :email, :session_token, presence: true, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    generate_unique_session_token unless self.session_token
+  end
 end
