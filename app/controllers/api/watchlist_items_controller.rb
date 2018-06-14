e5af68d87730122a7e98016fa91973d18951f77d
Stephen <kang.stephen94@gmail.com>
2018-06-13 19:41:29 -0700

    pre-rollback

--- app/controllers/api/watchlist_items_controller.rb
+++ app/controllers/api/watchlist_items_controller.rb
@@ -1,8 +1,8 @@
 class Api::WatchlistItemsController < ApplicationController
   def create
-    asset_id = params[:asset].id
+    asset_id = params[:asset][:id].to_i
     watchlist_id = current_user.id
-    @watchlist_item = WatchListItem.new({asset_id: asset_id, watchlist_id: watchlist_id})
+    @watchlist_item = WatchlistItem.new({asset_id: asset_id, watchlist_id: watchlist_id})
     if @watchlist_item.save
     else
       render json: ["This company is already in your watchlist!"], status: 401
