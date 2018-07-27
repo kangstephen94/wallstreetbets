991eb29702b0e6b4910cc07aea88947c42193f11
Stephen Kang <kang.stephen94@gmail.com>
2018-07-27 11:31:02 -0700

    Take out logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
@@ -8,7 +7,7 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(logger)
   )
 );
 
