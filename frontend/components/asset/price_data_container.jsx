1cc72501e5601ad9b3f1c64bbafeb84d03e533f9
Stephen Kang <kang.stephen94@gmail.com>
2018-07-27 11:26:34 -0700

    Change sign up  css

--- frontend/components/asset/price_data_container.jsx
+++ frontend/components/asset/price_data_container.jsx
@@ -1,19 +1,19 @@
 import PriceDate from './price_data';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
   let endPrice;
   let startPrice;
- const prices = state.entities.data.data;
- if (prices) {
- endPrice = prices[Object.keys(prices).length-1].value;
- startPrice = prices[0].value;
-}
- return {
-   pointedPrice: state.entities.tooltip,
-   endPrice: endPrice,
-   startPrice: startPrice
-};
+  const prices = state.entities.data.data;
+  if (prices) {
+    endPrice = prices[Object.keys(prices).length - 1].value;
+    startPrice = prices[0].value;
+  }
+  return {
+    pointedPrice: state.entities.tooltip,
+    endPrice: endPrice,
+    startPrice: startPrice
+  };
 };
 
 
